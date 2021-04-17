d8b718c14f26045facc8fa00aa333db595ad1b17
Miguel Escribano (miguescri) <miguescri@gmail.com>
2021-04-17 21:11:43 +0200

    Use containerd as runtime in kube-base

--- vagrant_kube_base/provision.sh
+++ vagrant_kube_base/provision.sh
@@ -1,15 +1,41 @@
 #!/bin/bash -x
 
+# Configure kernel
+cat <<EOF > /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF > /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+# Add official Kubernetes repository
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat << EOF > /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/  kubernetes-xenial main
 EOF
 
+# Add official Docker repository
+curl -s https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+cat << EOF > /etc/apt/sources.list.d/docker.list
+deb https://download.docker.com/linux/ubuntu bionic	 stable
+EOF
+
+# Install packages 
 apt update -y
-
-apt install -y docker.io
-systemctl enable docker.service
-
+apt install -y containerd.io
 apt install -y kubeadm=1.19.1-00 kubelet=1.19.1-00 kubectl=1.19.1-00
 apt-mark hold kubeadm kubelet kubectl
+
+# Configure containerd
+containerd config default > /etc/containerd/config.toml
+systemctl restart containerd
 
--- vagrant_kube_base/run.sh
+++ vagrant_kube_base/run.sh
@@ -1,4 +1,6 @@
+rm kube-base.box
+vagrant destroy -f
 vagrant up
 vagrant halt
 vagrant package --output=kube-base.box kube-base
-vagrant box add kube-base.box --name=kube-base
+vagrant box add kube-base.box --name=kube-base --force
